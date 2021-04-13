c6dceeb2a2f552f28398a0758e859b81c018e25d
Andreas <andreas.grip@gmail.se>
2021-04-14 00:09:32 +0200

    Init template module

---
+++ modules/module_template/api/route.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const path = require('path');
+
+router.use('/firstname', (req,res,next) => {
+    res.end('John');
+});
+
+router.use('/lastname', (req,res,next) => {
+    res.end('Doe');
+});
+
+router.get('/', (req, res, next) => {
+    res.render(path.join(__dirname, 'view'));
+  });
+
+module.exports = router;
---
+++ modules/module_template/route.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const path = require('path');
+const fs = require('fs');
+
+fs.readdirSync(__dirname, { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .forEach((folder) => {
+    if (fs.existsSync(path.join(__dirname, folder.name, 'route.js'))) {
+      console.log('Loading module from ' + path.join(__dirname, folder.name, 'route.js'))
+      router.use('/' + folder.name, require(path.join(__dirname, folder.name, 'route.js')));
+    }
+  });
+
+router.use('/secret', (req, res, next) => {
+  res.end('Nothing to se here, please go away!');
+});
+router.get('/', (req, res, next) => {
+  res.render(path.join(__dirname, 'view'));
+});
+// etc.
+
+module.exports = router;
